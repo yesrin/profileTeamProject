3e1cdec3dc8de57580a1fedf20585e950477d57f
AAARRR0412 <anrobot0412@gmail.com>
2023-05-17 20:40:50 +0900

    코드 간소화 및 카드 디자인 변경

--- app.py
+++ app.py
@@ -43,26 +43,5 @@ def home():
 def start():
     return render_template('index2.html')
 
-# @app.route("/mars", methods=["POST"])
-# def mars_post():
-#     name_receive = request.form['name_give']
-#     address_receive = request.form['address_give']
-#     size_receive = request.form['size_give']
-
-#     doc = {
-#         'name': name_receive,
-#         'address': address_receive,
-#         'size': size_receive
-#     }
-
-#     db.mars.insert_one(doc)
-
-#     return jsonify({'msg': '주문 완료!'})
-
-# @app.route("/mars", methods=["GET"])
-# def mars_get():
-#     mars_data = list(db.mars.find({},{'_id':False}))
-#     return jsonify({'result':mars_data})
-
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
